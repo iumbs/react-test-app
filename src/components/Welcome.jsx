0d68c8053d5f4452821a7a22e4d3204fb15c7dd0
Umberto Poli <umberto.poli1@gmail.com>
2017-10-02 02:52:28 +0200

    Fix some linting problems

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,42 +1,42 @@
 import React from 'react';
 
 export const Clock = (props) => {
-  let fullDateToString = props.date.toLocaleTimeString();
-  let dateToString = fullDateToString.substr(0,5);
-  return <h1>{fullDateToString}</h1>
+	let fullDateToString = props.date.toLocaleTimeString();
+	let dateToString = fullDateToString.substr(0,5);
+	return <h1>{fullDateToString}</h1>;
 };
 
 export class Hello extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.handleNameInputChange = this.handleNameInputChange.bind(this);
-    this.handleNameSubmit = this.handleNameSubmit.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.handleNameInputChange = this.handleNameInputChange.bind(this);
+		this.handleNameSubmit = this.handleNameSubmit.bind(this);
+	}
 
-  handleNameInputChange(e) {
-		this.props.onNameInputChange(e.target.value)
+	handleNameInputChange(e) {
+		this.props.onNameInputChange(e.target.value);
 	}
 
 	handleNameSubmit(e) {
-    this.props.onNameSubmit(true)
+		this.props.onNameSubmit(true);
 	}
 
-  render() {
-    if (!this.props.showName) {
-      return (
-        <h2>Hello,
-          <form onSubmit={this.handleNameSubmit}>
-            <input type="text" value={this.props.name} onChange={this.handleNameInputChange} />
-            <input type="submit" value="⮐" />
-          </form>
-        </h2>
-      );
-    } else {
-      return (
-        <h2>Hello, {this.props.name}</h2>
-      );
-    }
-  }
+	render() {
+		if (!this.props.showName) {
+			return (
+				<h2>Hello,
+					<form onSubmit={this.handleNameSubmit}>
+						<input type="text" value={this.props.name} onChange={this.handleNameInputChange} />
+						<input type="submit" value="⮐" />
+					</form>
+				</h2>
+			);
+		} else {
+			return (
+				<h2>Hello, {this.props.name}</h2>
+			);
+		}
+	}
 
 }
