f20cd06c59d96e65bb36cad132f95040abab190f
Umberto Poli <umberto.poli1@gmail.com>
2017-10-02 03:01:37 +0200

    Fix some linting problems

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 export const TodoPrompt = (props) => {
 
-  return (
-    <div>
-      <h3>What's your main focus for today?</h3>
-      <input
-        type="text"
-        placeholder="Eg. Learn React"
-      />
-    </div>
-  );
+	return (
+		<div>
+			<h3>What's your main focus for today?</h3>
+			<input
+				type="text"
+				placeholder="Eg. Learn React"
+			/>
+		</div>
+	);
 
-}
+};
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,107 +2,107 @@ import React from 'react';
 
 export class Clock extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.toggleSeconds = this.toggleSeconds.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.toggleSeconds = this.toggleSeconds.bind(this);
+	}
 
-  toggleSeconds(e) {
-    const bool = this.props.seconds ? false : true;
-    this.props.onSecondsToggler(bool);
-  }
+	toggleSeconds(e) {
+		const bool = this.props.seconds ? false : true;
+		this.props.onSecondsToggler(bool);
+	}
 
-  render() {
-    const fullDate = this.props.date.toLocaleTimeString();
-    const date = fullDate.substr(0, 5);
-    if (this.props.seconds) {
-      return (
-        <div>
-          <h1>{fullDate}</h1>
-          <button onClick={this.toggleSeconds}>Hide seconds</button>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <h1>{date}</h1>
-          <button onClick={this.toggleSeconds}>Show seconds</button>
-        </div>
-      );
-    }
-  }
+	render() {
+		const fullDate = this.props.date.toLocaleTimeString();
+		const date = fullDate.substr(0, 5);
+		if (this.props.seconds) {
+			return (
+				<div>
+					<h1>{fullDate}</h1>
+					<button onClick={this.toggleSeconds}>Hide seconds</button>
+				</div>
+			);
+		} else {
+			return (
+				<div>
+					<h1>{date}</h1>
+					<button onClick={this.toggleSeconds}>Show seconds</button>
+				</div>
+			);
+		}
+	}
 
 }
 
 export class Hello extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.handleNameInputChange = this.handleNameInputChange.bind(this);
-    this.toggleNameSubmit = this.toggleNameSubmit.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.handleNameInputChange = this.handleNameInputChange.bind(this);
+		this.toggleNameSubmit = this.toggleNameSubmit.bind(this);
+	}
 
-  handleNameInputChange(e) {
-    const name = e.target.value;
-    const capitalizedName = name.charAt(0).toUpperCase() + name.slice(1);
+	handleNameInputChange(e) {
+		const name = e.target.value;
+		const capitalizedName = name.charAt(0).toUpperCase() + name.slice(1);
 		this.props.onNameInputChange(capitalizedName);
 	}
 
 	toggleNameSubmit(e) {
-    if (this.props.name.value) {
-      const bool = this.props.name.show ? false : true;
-      this.props.onNameSubmit(bool);
-      e.preventDefault();
-    } else {
-      e.preventDefault();
-    }
+		if (this.props.name.value) {
+			const bool = this.props.name.show ? false : true;
+			this.props.onNameSubmit(bool);
+			e.preventDefault();
+		} else {
+			e.preventDefault();
+		}
 	}
 
-  render() {
-    if (!this.props.name.show && !this.props.name.value) {
-      return (
-        <h2>{this.props.welcomeMessage},
-          <form onSubmit={this.toggleNameSubmit}>
-            <input
-              type="text"
-              placeholder="Your name..."
-              value={this.props.name.value}
-              onChange={this.handleNameInputChange}
-            />
-          </form>
-        </h2>
-      );
-    } else if (!this.props.name.show && this.props.name.value) {
-        return (
-          <h2>{this.props.welcomeMessage},
-            <form onSubmit={this.toggleNameSubmit}>
-              <input
-                type="text"
-                placeholder="Your name..."
-                value={this.props.name.value}
-                onChange={this.handleNameInputChange}
-              />
-              <input
-                type="submit"
-                value="⮐"
-              />
-            </form>
-          </h2>
-        );
-    } else {
-      return (
-        <div>
-          <h2>
-            {this.props.welcomeMessage}, {this.props.name.value}.
-            <form onSubmit={this.toggleNameSubmit}>
-              <input
-                type="submit"
-                value="Reset name"
-              />
-            </form>
-          </h2>
-        </div>
-      );
-    }
-  }
+	render() {
+		if (!this.props.name.show && !this.props.name.value) {
+			return (
+				<h2>{this.props.welcomeMessage},
+					<form onSubmit={this.toggleNameSubmit}>
+						<input
+							type="text"
+							placeholder="Your name..."
+							value={this.props.name.value}
+							onChange={this.handleNameInputChange}
+						/>
+					</form>
+				</h2>
+			);
+		} else if (!this.props.name.show && this.props.name.value) {
+			return (
+				<h2>{this.props.welcomeMessage},
+					<form onSubmit={this.toggleNameSubmit}>
+						<input
+							type="text"
+							placeholder="Your name..."
+							value={this.props.name.value}
+							onChange={this.handleNameInputChange}
+						/>
+						<input
+							type="submit"
+							value="⮐"
+						/>
+					</form>
+				</h2>
+			);
+		} else {
+			return (
+				<div>
+					<h2>
+						{this.props.welcomeMessage}, {this.props.name.value}.
+						<form onSubmit={this.toggleNameSubmit}>
+							<input
+								type="submit"
+								value="Reset name"
+							/>
+						</form>
+					</h2>
+				</div>
+			);
+		}
+	}
 }
